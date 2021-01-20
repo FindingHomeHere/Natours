dc20ad896b5c75c7ff3f62df74fc28a912e32667
Isaac Johnson <isaac.johnson.2493@gmail.com>
2021-01-19 17:26:59 -0700

    prepared app for deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -7,10 +7,8 @@ const stripe = Stripe(process.env.STRIPE_PUBLIC_KEY);
 export const bookTour = async tourId => {
   try {
     // Get checkout session from api
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
-    console.log(session);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    // console.log(session);
 
     // create checkout form + charge card
     await stripe.redirectToCheckout({
